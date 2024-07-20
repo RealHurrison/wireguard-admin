f014cf7bfafc1409a01c2a502cdd7f47d3fbe19e
Hurrison <admin@hurrison.com>
2024-07-20 12:04:47 +0800

    feat: define model

--- main.go
+++ main.go
@@ -1,30 +1,71 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-type User struct {
-	gorm.Model
-	UserName string `json:"username"`
-	PassWord string `json:"password"`
+type Model struct {
+	ID        string `gorm:"type:uuid;primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
-type UserLoginRequest struct {
-	UserName string `json:"username"`
-	PassWord string `json:"password"`
+type Config struct {
+	Subnet    string
+	Interface string
 }
 
-func main() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+type Admin struct {
+	Model
+	UserName string
+	PassWord string
+}
+
+type Client struct {
+	Model
+	Name       string
+	PrivateKey string
+	PublicKey  string
+	IP         string
+	DNS        string
+	Route      string
+	Rules      []Rule
+}
+
+type Rule struct {
+	Model
+	ClientID string
+	Protocol string
+	DestIP   string
+	DestPort string
+	Action   string
+}
+
+type LoginRequest struct {
+	UserName string
+	PassWord string
+}
+
+var DB *gorm.DB
+
+func initDatabase() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("data.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	db.AutoMigrate(&User{})
-	db.Create(&User{UserName: "admin", PassWord: "admin"})
+	DB.AutoMigrate(&Admin{})
+}
+
+func main() {
+	initDatabase()
+	DB.Create(&Admin{UserName: "admin", PassWord: "admin"})
 
 	r := gin.Default()
 	r.GET("/api/ping", func(c *gin.Context) {
@@ -34,14 +75,14 @@ func main() {
 	})
 
 	r.POST("/api/login", func(c *gin.Context) {
-		var userLoginRequest UserLoginRequest
+		var userLoginRequest LoginRequest
 		if err := c.ShouldBindJSON(&userLoginRequest); err != nil {
 			c.JSON(400, gin.H{"error": err.Error()})
 			return
 		}
 
-		var user User
-		db.Where("user_name = ?", userLoginRequest.UserName).First(&user)
+		var user Admin
+		DB.Where("user_name = ?", userLoginRequest.UserName).First(&user)
 		if user.PassWord == userLoginRequest.PassWord {
 			c.JSON(200, gin.H{"message": "login success"})
 		} else {
