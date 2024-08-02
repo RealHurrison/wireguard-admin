76493b50da1962bfba0167abf5b28ff8f42ffb02
Hurrison <admin@hurrison.com>
2024-08-02 10:09:36 +0800

    feat: design and deploy favicon package

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import wireguardLogo from './assets/wireguard.svg'
 import './App.css'
 
 function App() {
@@ -9,14 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://www.wireguard.com" target="_blank">
+          <img src={wireguardLogo} className="logo" alt="Wireguard logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Wireguard + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Wireguard and React logos to learn more
       </p>
     </>
   )
